7d903c807dea0b16faebf8bef6e25183c96bcd41
Jett Thompson <jett.thompson.59@gmail.com>
2021-08-10 18:27:57 +1000

    made the pdf view allow the user to select a pdf file to open and made it prn the number of pages in the file

--- src/main/java/app/cleancode/java_pdf_viewer/ui/Main.java
+++ src/main/java/app/cleancode/java_pdf_viewer/ui/Main.java
@@ -1,14 +1,17 @@
 package app.cleancode.java_pdf_viewer.ui;
 
+import app.cleancode.java_pdf_viewer.ui.pdf.PDFView;
 import app.cleancode.java_pdf_viewer.ui.toolbar.ToolbarView;
 import javafx.application.Application;
 import javafx.scene.AccessibleRole;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public static Stage top;
 
     public static void main(String[] args) {
         launch(args);
@@ -16,12 +19,17 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        top = primaryStage;
         ToolbarView toolbar = new ToolbarView();
         toolbar.setAccessibleRole(AccessibleRole.TOOL_BAR);
         BorderPane root = new BorderPane();
         root.setTop(toolbar);
+        PDFView.INSTANCE.addListener((observable, newValue, oldValue) -> {
+            root.setCenter(newValue);
+        });
         primaryStage.setScene(new Scene(root));
         primaryStage.setTitle("Pdf Viewer");
+        primaryStage.setMaximized(true);
         primaryStage.show();
     }
 
--- src/main/java/app/cleancode/java_pdf_viewer/ui/pdf/PDFView.java
+++ src/main/java/app/cleancode/java_pdf_viewer/ui/pdf/PDFView.java
@@ -1,13 +1,60 @@
 package app.cleancode.java_pdf_viewer.ui.pdf;
 
+import app.cleancode.java_pdf_viewer.ui.Main;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 public class PDFView extends TextArea {
+    public static ObjectProperty<PDFView> INSTANCE = new SimpleObjectProperty<>();
+
     public static void open() {
-        return;
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setSelectedExtensionFilter(new ExtensionFilter("PDF", List.of("pdf")));
+        File file = fileChooser.showOpenDialog(Main.top);
+        FileInputStream fileIn = null;
+        PdfReader pdfReader = null;
+        PdfDocument pdf = null;
+        try {
+            fileIn = new FileInputStream(file);
+            pdfReader = new PdfReader(fileIn);
+            pdf = new PdfDocument(pdfReader);
+            INSTANCE.set(new PDFView(pdf));
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (fileIn != null) {
+                try {
+                    fileIn.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                if (pdfReader != null) {
+                    try {
+                        pdfReader.close();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                    if (pdf != null) {
+                        pdf.close();
+                    }
+                }
+            }
+        }
     }
 
-    public PDFView() {
+    private final PdfDocument pdf;
+
+    public PDFView(PdfDocument pdf) {
+        this.pdf = pdf;
+        System.out.println(pdf.getNumberOfPages());
         super.setEditable(false);
     }
 }
