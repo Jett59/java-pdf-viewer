4f485571476e69be83b3d298681a88029569066a
Jett Thompson <jett.thompson.59@gmail.com>
2021-08-12 16:00:11 +1000

    added a package script that does not work yet

---
+++ package_win64.sh
@@ -0,0 +1,20 @@
+set -e
+set +x
+
+mvn clean package
+
+jpackage \
+--type msi \
+-d ./target/installer \
+--name "Pdf Viewer" \
+--input ./target/app \
+--app-version "0.1" \
+--copyright "2021 Jett Thompson" \
+--vendor "Jett Thompson" \
+--main-jar java-pdf-viewer.jar \
+--module-path ./target/app/lib \
+--add-modules javafx.controls \
+--win-shortcut \
+--win-menu \
+--win-dir-chooser \
+--win-upgrade-uuid "afd078bf-cc02-47f8-8cba-c5dbdf33e1fe" \
