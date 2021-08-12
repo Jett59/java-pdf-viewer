044b1a3860b2672b9859097a6d93216f35733eb6
Jett Thompson <jett.thompson.59@gmail.com>
2021-08-12 17:27:22 +1000

    made some changes to the pom and made the package script work properly

--- package_win64.sh
+++ package_win64.sh
@@ -6,15 +6,15 @@ mvn clean package
 jpackage \
 --type msi \
 -d ./target/installer \
---name "Pdf Viewer" \
+--name "PdfViewer" \
 --input ./target/app \
 --app-version "0.1" \
 --copyright "2021 Jett Thompson" \
 --vendor "Jett Thompson" \
 --main-jar java-pdf-viewer.jar \
 --module-path ./target/app/lib \
---add-modules javafx.controls \
+--add-modules javafx.controls,java.logging \
 --win-shortcut \
 --win-menu \
 --win-dir-chooser \
---win-upgrade-uuid "afd078bf-cc02-47f8-8cba-c5dbdf33e1fe" \
+--win-upgrade-uuid "afd078bf-cc02-47f8-8cba-c5dbdf33e1fe"
