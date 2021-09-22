868ee544011899fb178529e7e10f84ca13ff57b0
Jett Thompson <jett.thompson.59@gmail.com>
2021-09-22 15:25:52 +1000

    incremented the version to 0.3.3

--- package_win64.sh
+++ package_win64.sh
@@ -8,7 +8,7 @@ jpackage \
 -d ./target/installer \
 --name "PdfViewer" \
 --input ./target/app \
---app-version "0.3.2" \
+--app-version "0.3.3" \
 --copyright "2021 Jett Thompson" \
 --vendor "Jett Thompson" \
 --license-file LICENSE \
